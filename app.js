1da4b9e6d39492ec4a93c5d2a967c57874c673d3
AdrianMSS-XYZ <asanchez@imaginexyz.com>
2023-05-11 11:24:21 -0600

    [Update] - CORS && Winston

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require("express");
+var cors = require('cors');
+const winston = require("winston");
 const app = express();
 const port = process.env.PORT || 3001;
 
 let lightState = 'off';
 let phValue = '';
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+
+const logger = winston.createLogger({
+  // Log only if level is less than (meaning more severe) or equal to this
+  level: "info",
+  // Use timestamp and printf to create a standard log format
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  ),
+  // Log to the console and a file
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ],
+});
+app.use((req, res, next) => {
+  // Log an info message for each incoming request
+  logger.info(`Received a ${req.method} request for ${req.url}`);
+  next();
+});
 
 app.get('/all', (req, res) => {
   res.send({light:lightState, ph:phValue})
