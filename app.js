a1156b895d93a13cc975113a5c2dad3b65cf1bb2
AdrianMSS-XYZ <asanchez@imaginexyz.com>
2023-05-12 09:12:48 -0600

    [Fix] - Next on CORS

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.all('*', function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  //...
+  next();
  });
 
 const logger = winston.createLogger({
